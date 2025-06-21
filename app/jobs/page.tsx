48372d426e961f51a794fc98fb90cad5ea157b54
Maksymilian <max.szewczyk@interia.pl>
2025-06-21 13:50:02 +0200

    .

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -2,14 +2,16 @@
 import PageTitle from "@/components/ui/PageTitle";
 import LocalJobSearcher from "@/components/ui/search/LocalJobSearcher";
 import { useSidebarContext } from "context/LeftSidebarContext";
-import React from "react";
+import React, { Suspense } from "react";
 
 const Page = () => {
     const { isCompact } = useSidebarContext();
     return (
         <div className="wrapper">
             <PageTitle title="Oferty pracy" />
-            <LocalJobSearcher />
+            <Suspense>
+                <LocalJobSearcher />
+            </Suspense>
             <div
                 className={`grid grid-cols-1 gap-3 sm:grid-cols-2 ${isCompact ? "md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6" : "lg:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-6"}`}></div>
         </div>
